37b3b1d39063f8ca015d1c6c3a288467ab8afc28
Aimar Abimayu Pratama <aimarabimanyu123@gmail.com>
2022-09-30 22:17:19 +0700

    csv generator

--- AudioPreprocessingDataset.py
+++ AudioPreprocessingDataset.py
@@ -1,8 +1,10 @@
 import os
+from re import L
 import librosa
 import json
 import math
 import warnings
+import csv
 
 warnings.filterwarnings('ignore')
 DATASET_PATH = "Dataset"
@@ -24,7 +26,7 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
         """
 
     # dictionary to store mapping, labels, and MFCCs
-    data = {
+    dataset = {
         "mapping": [],
         "labels": [],
         "mfcc": [],
@@ -33,7 +35,7 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
 
     samples_per_segment = int(SAMPLES_PER_SOUND / num_segments)
     num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
-
+    
     # loop through all genre sub-folder
     for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
 
@@ -42,14 +44,14 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
 
             # save genre label (i.e., sub-folder name) in the mapping
             semantic_label = dirpath.split("/")[-1]
-            data["mapping"].append(semantic_label)
+            dataset["mapping"].append(semantic_label)
             print("\nProcessing: {}".format(semantic_label))
 
             # process all audio files in genre sub-dir
             for f in filenames:
                 file_path = os.path.join(dirpath, f)
 
- # load audio file and slice it to ensure length consistency among different files
+                # load audio file and slice it to ensure length consistency among different files
                 signal, sample_rate = librosa.load(file_path)
 
                 # drop audio files with less than pre-decided number of samples
@@ -63,16 +65,34 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
                                                  hop_length=hop_length)
 
                     # store data for analysed track
-                    data["mfcc"].append(MFCCs.T.tolist())
-                    data["labels"].append(i-1)
-                    data["files"].append(file_path)
+                    dataset["mfcc"].append(MFCCs.T.tolist())
+                    dataset["labels"].append(i-1)
+                    dataset["files"].append(file_path)
                     print("{}: {}".format(file_path, i-1))
 
 
     # save MFCCs to json file
     with open(json_path, "w") as fp:
-        json.dump(data, fp, indent=4)
-        
+        json.dump(dataset, fp, indent=4)
+
+    # save MFCCs to csv file
+    j = len(dataset["labels"])
+    i = 0
+    temp = []
+    
+    while i < j:
+        temp1 = []
+        temp1.extend([dataset["labels"][i]])
+        temp1.extend([dataset["files"][i]])
+        temp1.extend([dataset["mfcc"][i]])
+        temp.append(temp1)
+        i += 1
+
+    with open('data.csv', 'w') as file:
+        writer = csv.writer(file)
+
+        writer.writerows(map(lambda x: [x], temp))
+
         
 if __name__ == "__main__":
     save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
